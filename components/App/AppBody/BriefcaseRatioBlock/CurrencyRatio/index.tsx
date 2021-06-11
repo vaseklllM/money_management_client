125e50cad7a806c72fad4b6b1555d2339e34a952
vasek <vasia.motnin@gmail.com>
2021-06-11 16:16:21 +0300

    update

--- components/App/AppBody/BriefcaseRatioBlock/CurrencyRatio/index.tsx
+++ components/App/AppBody/BriefcaseRatioBlock/CurrencyRatio/index.tsx
@@ -1,19 +1,82 @@
 import React, { ReactElement } from "react"
 import CurrencyRatioBody from "./CurrencyRatioBody"
 import { ICurrencyRatioData } from "./interface"
+import CURRENCY_ACCOUNTS from "./currencyAccounts.gql"
+import BANK_CARDS from "./bankcards.gql"
+import { useQuery } from "@apollo/client"
 
-interface Props {}
+interface ICurrency {
+  code: string
+  symbol: string
+  historyCourseInUAH: {
+    price: number
+  }[]
+}
 
-export default function CurrencyRatio({}: Props): ReactElement {
-  const data: ICurrencyRatioData[] = [
-    { symbol: "$", code: "USD", value: 123 },
-    { symbol: "₴", code: "UAH", value: 456 },
-    { symbol: "€", code: "EUR", value: 789 },
-  ]
+interface ICA {
+  currencyAccounts: {
+    id: string
+    value: number
+    currency: ICurrency
+  }[]
+}
+
+interface IBC {
+  bankcards: {
+    monobank: {
+      historyCards: {
+        cards: {
+          balance: number
+          currency: ICurrency
+        }[]
+      }[]
+    }
+  }
+}
+
+export default function CurrencyRatio(): ReactElement {
+  const { data: CAData, loading: CALoading } = useQuery<ICA>(CURRENCY_ACCOUNTS)
+  const { data: BCData, loading: BCLoading } = useQuery<IBC>(BANK_CARDS)
+
+  if (CALoading || BCLoading) return null
+
+  const data: ICurrencyRatioData[] = convertBCData(BCData, convertCAData(CAData)).filter(
+    (i) => i.value > 0
+  )
+
+  if (data.length < 2) return null
 
   return (
     <div>
       <CurrencyRatioBody data={data} />
     </div>
   )
+}
+
+function convertCAData(CAData: ICA): ICurrencyRatioData[] {
+  let data: ICurrencyRatioData[] = []
+
+  CAData.currencyAccounts.forEach((el) => {
+    const dataEl = data.find((i) => i.symbol === el.currency.symbol)
+    if (!dataEl) {
+      data.push({ code: el.currency.code, symbol: el.currency.symbol, value: el.value })
+    } else {
+      dataEl.value = dataEl.value + el.value
+    }
+  })
+
+  return data
+}
+
+function convertBCData(BCData: IBC, data: ICurrencyRatioData[]): ICurrencyRatioData[] {
+  BCData.bankcards.monobank.historyCards[0].cards.forEach((el) => {
+    const dataEl = data.find((i) => i.symbol === el.currency.symbol)
+    if (!dataEl) {
+      data.push({ code: el.currency.code, symbol: el.currency.symbol, value: el.balance })
+    } else {
+      dataEl.value = dataEl.value + el.balance
+    }
+  })
+
+  return data
 }
