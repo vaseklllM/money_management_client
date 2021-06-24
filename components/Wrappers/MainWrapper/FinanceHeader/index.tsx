ad565aaea3b43bfe1dd7742f41e6fc55353de790
vasek <vasia.motnin@gmail.com>
2021-06-24 23:18:20 +0300

    update header

--- components/Wrappers/MainWrapper/FinanceHeader/index.tsx
+++ components/Wrappers/MainWrapper/FinanceHeader/index.tsx
@@ -10,6 +10,7 @@ interface Props {
 }
 
 export default function Header({ className }: Props): ReactElement {
+  const [scrollY, setScrollY] = useState<number>(0)
   const [fixed, setFixed] = useState<boolean>(false)
 
   useEffect(() => {
@@ -21,12 +22,14 @@ export default function Header({ className }: Props): ReactElement {
 
   function handleScroll() {
     setFixed(window.scrollY > 16)
+    if (window.scrollY <= 16) setScrollY(window.scrollY)
   }
 
   return (
     <div className={txt.join([className, classes.header_wrapper])}>
       <div
         className={txt.join([classes.header, fixed && classes.fixed_header, className])}
+        style={{ top: fixed ? 0 : `${16 - scrollY}px` }}
       >
         <FinanceHeaderTitle className={classes.currencies} />
         <LogoutButton className={classes.logout} />
