7cddb8e5b6babc1c944aa272a9f2cb9777f96ba9
vasek <vasia.motnin@gmail.com>
2021-06-25 21:42:19 +0300

    Update main page

--- components/Wrappers/MainWrapper/SideMenu/index.tsx
+++ components/Wrappers/MainWrapper/SideMenu/index.tsx
@@ -1,91 +1,30 @@
 import React, { memo } from "react"
-// import { Layout, /* Menu */ } from "antd"
-// import { useRouter } from "next/router"
-// import { /* BitcoinIcon, */ BriefcaseIcon } from "@/components/Icons"
-// import { DollarCircleOutlined /* , BarChartOutlined  */ } from "@ant-design/icons"
 import SideMenuLink from "./SideMenuLink"
 import classes from "./style.module.scss"
 import briefcase from "./icons/briefcase.svg"
 import currency from "./icons/currency.svg"
+import bitcoin from "./icons/bitcoin.svg"
+import chart from "./icons/bar-chart.svg"
 import SideMenuButtonOpen from "./SideMenuButtonOpen"
 import { useSideMenuChangeOpen } from "./hooks/useSideMenuChangeOpen"
 import { txt } from "@/utils"
 
-// const { Sider } = Layout
 interface Props {
   className?: string
 }
 
 export default memo(function SideMenu({ className }: Props) {
   const { open, setOpen } = useSideMenuChangeOpen()
-  // const router = useRouter()
 
   return (
     <div className={txt.join([classes.body, !open && classes.close_body, className])}>
       <div className={classes.links}>
         <SideMenuLink text='Портфель' to='/' icon={briefcase} />
         <SideMenuLink text='Валюти' to='/currencies' icon={currency} />
+        <SideMenuLink text='Акції' to='/promotions' icon={chart} />
+        <SideMenuLink text='Криптовалюта' to='/cryptocurrency' icon={bitcoin} />
       </div>
       <SideMenuButtonOpen open={open} setOpen={setOpen} />
     </div>
   )
-
-  // const financeMenuList = [
-  //   {
-  //     id: "briefcase",
-  //     name: "Портфель",
-  //     path: "/",
-  //     url: "/",
-  //     icon: <BriefcaseIcon />,
-  //   },
-  //   {
-  //     id: "currencies",
-  //     name: "Валюти",
-  //     path: "/currencies",
-  //     url: "/currencies",
-  //     icon: <DollarCircleOutlined />,
-  //   },
-  //   // {
-  //   //   id: "actions",
-  //   //   name: "Акції",
-  //   //   path: "/promotions",
-  //   //   url: "/promotions",
-  //   //   icon: <BarChartOutlined />,
-  //   // },
-  //   // {
-  //   //   id: "cryptocurrency",
-  //   //   name: "Криптовалюта",
-  //   //   path: "/cryptocurrency",
-  //   //   url: "/cryptocurrency",
-  //   //   icon: <BitcoinIcon />,
-  //   // },
-  // ]
-
-  // return (
-  //   <Sider
-  //     className={className}
-  //     theme='light'
-  //     collapsible
-  //     collapsed={collapsed}
-  //     onCollapse={setCollapsed}
-  //   >
-  //     <div className='logo' />
-  //     <Menu
-  //       theme='light'
-  //       defaultSelectedKeys={["briefcase"]}
-  //       mode='inline'
-  //       onClick={(e) => {
-  //         const newUrl = financeMenuList.find((i) => i.id === e.key).url
-  //         if (router.pathname !== newUrl) router.push(newUrl)
-  //       }}
-  //       selectedKeys={[financeMenuList.find((i) => i.path === router.route).id]}
-  //     >
-  //       {financeMenuList.map((el) => (
-  //         <Menu.Item key={el.id} icon={el.icon}>
-  //           {el.name}
-  //         </Menu.Item>
-  //       ))}
-  //     </Menu>
-  //   </Sider>
-  // )
 })
