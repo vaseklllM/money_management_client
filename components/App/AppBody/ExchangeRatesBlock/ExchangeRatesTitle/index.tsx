7a2ec9ad5e831c1074983a6fb6c0573592c31d49
vasek <vasia.motnin@gmail.com>
2021-07-18 17:44:40 +0300

    update ExchangeRatesTitle

--- components/App/AppBody/ExchangeRatesBlock/ExchangeRatesTitle/index.tsx
+++ components/App/AppBody/ExchangeRatesBlock/ExchangeRatesTitle/index.tsx
@@ -1,6 +1,4 @@
 import { Span14bold } from "@/components/Typography"
 import React, { ReactElement } from "react"
 
-const ExchangeRatesTitle = (): ReactElement => <Span14bold>Курс валют</Span14bold>
-
-export default ExchangeRatesTitle
+export const ExchangeRatesTitle = (): ReactElement => <Span14bold>Курс валют</Span14bold>
--- components/App/AppBody/ExchangeRatesBlock/index.tsx
+++ components/App/AppBody/ExchangeRatesBlock/index.tsx
@@ -1,7 +1,7 @@
 import ContentBlock from "@/components/finance/ContentBlock"
 import React, { ReactElement } from "react"
 import ExchangeRatesBody from "./ExchangeRatesBody"
-import ExchangeRatesTitle from "./ExchangeRatesTitle"
+import { ExchangeRatesTitle } from "./ExchangeRatesTitle"
 
 export default function ExchangeRatesBlock(): ReactElement {
   return (
