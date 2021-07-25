105ae92acc870a7ebf60de1c0630360182acc760
vasek <vasia.motnin@gmail.com>
2021-07-25 17:04:16 +0300

    update BancCardIconIsNotValid

--- components/finance/BancCard/BankCardTitleRow/BancCardIconIsNotValid/index.tsx
+++ components/finance/BancCard/BankCardTitleRow/BancCardIconIsNotValid/index.tsx
@@ -2,23 +2,16 @@ import React, { memo, ReactElement } from "react"
 import { ReactSVG } from "react-svg"
 import icon from "./warning.svg"
 import classes from "./style.module.scss"
-import { Popover } from "antd"
-import { txt } from "@/utils"
+import { TooltipBlack } from "@/components/Tooltips"
 
 interface Props {
   className?: string
 }
 
-const content = (
-  <div>
-    <p>Помилка при спробі оновити дані</p>
-  </div>
-)
-
 export default memo(function BancCardIconIsNotValid({ className }: Props): ReactElement {
   return (
-    <Popover content={content} trigger='hover'>
-      <ReactSVG src={icon} className={txt.join([classes.icon, className])} />
-    </Popover>
+    <TooltipBlack className={className} title='Помилка при спробі оновити дані'>
+      <ReactSVG src={icon} className={classes.icon} />
+    </TooltipBlack>
   )
 })
