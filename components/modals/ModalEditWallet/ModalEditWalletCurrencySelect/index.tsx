ce68da82164f7bafa9b0b4d1f351b65d9570e771
vasek <vasia.motnin@gmail.com>
2021-08-08 12:27:42 +0300

    update select

--- components/modals/ModalEditWallet/ModalEditWalletCurrencySelect/index.tsx
+++ components/modals/ModalEditWallet/ModalEditWalletCurrencySelect/index.tsx
@@ -1,6 +1,6 @@
+import { Select } from "@/components/selects"
 import { Span14 } from "@/components/Typography"
 import { useQuery } from "@apollo/client"
-import { Select } from "antd"
 import React, { ReactElement } from "react"
 import { CURRENCIES } from "./currencies.gql"
 import classes from "./style.module.scss"
@@ -23,29 +23,21 @@ export default function ModalEditWalletCurrencySelect({
   currencyId,
   setCurrencyId,
 }: Props): ReactElement {
-  const { Option } = Select
-
   const { data, loading } = useQuery<ICurrenciesData>(CURRENCIES)
 
   if (loading) return null
-
-  const activeEl = data.currencies.find((i) => i.id === currencyId).code
 
   return (
     <div className={classes.body}>
       <Span14>Валюта</Span14>
       <Select
-        style={{ width: 120 }}
+        data={data.currencies.map((currency) => ({
+          id: currency.id,
+          name: currency.code,
+        }))}
+        value={data.currencies.find((i) => i.id === currencyId).id}
         onChange={setCurrencyId}
-        className={classes.select}
-        value={activeEl}
-      >
-        {data.currencies.map((el) => (
-          <Option key={el.id} value={el.id}>
-            {el.code}
-          </Option>
-        ))}
-      </Select>
+      />
     </div>
   )
 }
