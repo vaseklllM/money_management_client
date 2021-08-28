0d418f3c8cec002715e97923711f2151e252df4d
vasek <vasia.motnin@gmail.com>
2021-08-28 23:51:24 +0300

    fixed getFirstMessage

--- utils/err.ts
+++ utils/err.ts
@@ -1,12 +1,12 @@
 /** Повертає текст помилки */
 function getFirstMessage(error: any): string {
-  if (Array.isArray(error.graphQLErrors) && error.graphQLErrors.length > 0) {
-    const resMessages = error.graphQLErrors[0].extensions.exception?.response?.message
-    if (Array.isArray(resMessages) && resMessages.length > 0) {
-      return resMessages[0]
-    } else {
-      return error.graphQLErrors[0].message
-    }
+  if (!error.graphQLErrors || error.graphQLErrors.length === 0) return
+
+  const resMessages = error.graphQLErrors[0].extensions.response?.message
+  if (Array.isArray(resMessages) && resMessages.length > 0) {
+    return resMessages[0]
+  } else {
+    return error.graphQLErrors[0].message
   }
 }
 
